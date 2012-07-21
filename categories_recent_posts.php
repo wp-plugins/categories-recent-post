3d545b01411e3216928bf0213110d856782da8a6
drmen8x <drmen8x@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-21 04:03:04 +0000

    Cap nhat code
    git-svn-id: https://plugins.svn.wordpress.org/categories-recent-post/trunk@575376 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ categories_recent_posts.php
@@ -0,0 +1,271 @@
+<?php
+/**
+ * @package Categories Recent Post
+ * @version 1.0
+ */
+/*
+Plugin Name: Categories Recent Post
+Plugin URI: http://saokim.com.vn
+Description: A widget that can display posts from checked categories and thumbnail image if post have <strong>set featured image</strong>
+Author: Nguyen Duc Manh
+Version: 1.0
+Author URI: http://saokim.com.vn
+*/
+
+// Register thumbnail sizes.
+if ( function_exists('add_image_size') )
+{
+	$sizes = get_option('sk_cat_post_thumb_sizes');
+	if ( $sizes )
+	{
+		foreach ( $sizes as $id=>$size )
+			add_image_size( 'cat_post_thumb_size' . $id, $size[0], $size[1], true );
+	}
+}
+
+class Category_Recent_posts extends WP_Widget {
+	function Category_Recent_posts() {
+		parent::WP_Widget(false, $name='Categories Recent Post');
+	}
+	
+	function widget($args, $instance)
+	{
+		// outputs the content of the widget
+		extract( $args );
+		$sizes = get_option('sk_cat_post_thumb_sizes');
+		
+		// If not title, use the "Recent Post in categories".
+		if( !$instance["title"] ) {
+			$instance["title"] = "Recent Post in categories";
+	  	}
+		echo $before_widget;
+			echo $before_title . $instance["title"]. $after_title;
+			//Noi dung widget
+			$catArr	=	$instance["selected_cats"];
+			$strCat	=	"";
+			foreach($catArr as $value){
+				$strCat	.=	$value.",";		
+			}
+			$strCat	=	substr($strCat,0,strlen($strCat)-1);
+			
+			$valid_sort_orders = array('date', 'title', 'comment_count', 'random');
+			 if ( in_array($instance['sort_by'], $valid_sort_orders) ) {
+				$sort_by = $instance['sort_by'];
+				$sort_order = (bool) $instance['asc_sort_order'] ? 'ASC' : 'DESC';
+			  } else {
+				// by default, display latest first
+				$sort_by = 'date';
+				$sort_order = 'DESC';
+			 }
+			
+			$cat_posts = new WP_Query(
+				"showposts=" . $instance["num"] . 
+				"&cat=" . $strCat .
+				"&orderby=" . $sort_by .
+				"&order=" . $sort_order
+			  );
+			
+			// Post list
+			echo "<ul>\n";
+			
+			while ( $cat_posts->have_posts() )
+			{
+				$cat_posts->the_post();
+			?>
+				<li class="cat-post-item">
+					<?php
+						if (
+							function_exists('the_post_thumbnail') &&
+							current_theme_supports("post-thumbnails") &&
+							$instance["thumb"] &&
+							has_post_thumbnail()
+						) :
+					?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+						<?php the_post_thumbnail( 'cat_post_thumb_size'.$this->id, 'class=imgleft' ); ?>
+						</a>
+						 <a class="post-title" href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+						 <br clear="all" /><br />
+					<?php else:?>     
+					 <a class="post-title" href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+					<?php endif; ?>
+				</li>
+			<?php
+			}
+			
+			echo "</ul>\n";
+			
+		echo $after_widget;
+	}
+	
+	function form($instance) {
+		// outputs the options form on admin
+		$selected_cats	=	$instance["selected_cats"];	
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id("title"); ?>">
+				<?php _e( 'Title' ); ?>:
+				<input class="widefat" id="<?php echo $this->get_field_id("title"); ?>" name="<?php echo $this->get_field_name("title"); ?>" type="text" value="<?php echo esc_attr($instance["title"]); ?>" />
+			</label>
+		</p>
+        <p>
+			<label for="<?php echo $this->get_field_id("num"); ?>">
+				<?php _e('Number of posts to show'); ?>:
+				<input style="text-align: center;" id="<?php echo $this->get_field_id("num"); ?>" name="<?php echo $this->get_field_name("num"); ?>" type="text" value="<?php echo absint($instance["num"]); ?>" size='3' />
+			</label>
+   		 </p>
+
+        <p>
+           <label for="<?php echo $this->get_field_id("sort_by"); ?>">
+            <?php _e('Sort by'); ?>:
+            <select id="<?php echo $this->get_field_id("sort_by"); ?>" name="<?php echo $this->get_field_name("sort_by"); ?>">
+              <option value="date"<?php selected( $instance["sort_by"], "date" ); ?>>Date</option>
+              <option value="title"<?php selected( $instance["sort_by"], "title" ); ?>>Title</option>
+              <option value="comment_count"<?php selected( $instance["sort_by"], "comment_count" ); ?>>Number of comments</option>
+              <option value="random"<?php selected( $instance["sort_by"], "random" ); ?>>Random</option>
+            </select>
+                </label>
+        </p>
+		
+        <p>
+            <label for="<?php echo $this->get_field_id("asc_sort_order"); ?>">
+          <input type="checkbox" class="checkbox" 
+          id="<?php echo $this->get_field_id("asc_sort_order"); ?>" 
+          name="<?php echo $this->get_field_name("asc_sort_order"); ?>"
+          <?php checked( (bool) $instance["asc_sort_order"], true ); ?> />
+                <?php _e( 'Reverse sort order (ascending)' ); ?>
+            </label>
+         </p>
+         
+         
+         <?php if ( function_exists('the_post_thumbnail') && current_theme_supports("post-thumbnails") ) : ?>
+		<p>
+			<label for="<?php echo $this->get_field_id("thumb"); ?>">
+				<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("thumb"); ?>" name="<?php echo $this->get_field_name("thumb"); ?>"<?php checked( (bool) $instance["thumb"], true ); ?> />
+				<?php _e( 'Show post thumbnail' ); ?>
+			</label>
+		</p>
+		<p>
+			<label>
+				<?php _e('Thumbnail dimensions'); ?>:<br />
+				<label for="<?php echo $this->get_field_id("thumb_w"); ?>">
+					W: <input class="widefat" style="width:40%;" type="text" id="<?php echo $this->get_field_id("thumb_w"); ?>" name="<?php echo $this->get_field_name("thumb_w"); ?>" value="<?php echo $instance["thumb_w"]; ?>" />
+				</label>
+				
+				<label for="<?php echo $this->get_field_id("thumb_h"); ?>">
+					H: <input class="widefat" style="width:40%;" type="text" id="<?php echo $this->get_field_id("thumb_h"); ?>" name="<?php echo $this->get_field_name("thumb_h"); ?>" value="<?php echo $instance["thumb_h"]; ?>" />
+				</label>
+			</label>
+		</p>
+		<?php endif; ?>
+         
+        <?php
+				echo '			<b>Recent post in categories</b><hr />'; 
+				echo '			<ul id="categorychecklist" class="list:category categorychecklist form-no-clear" style="list-style-type: none; margin-left: 5px; padding-left: 0px; margin-bottom: 20px;">';
+
+				saokim_wp_category_checklist(0, 0, $selected_cats, false); 
+				echo '			</ul>'; 
+	}
+
+	function update($new_instance, $old_instance) {
+		// processes widget options to be saved
+		$instance = $old_instance;
+		/*$instance['title'] 		= strip_tags($new_instance['title']);*/
+		$instance['post_category']	=	serialize($_POST['post_category']);
+		$new_instance['selected_cats']	=	($instance['post_category'] != '') ? unserialize($instance['post_category']) : false;
+		if ( function_exists('the_post_thumbnail') )
+		{
+			$sizes = get_option('sk_cat_post_thumb_sizes'); 
+			if ( !$sizes ) $sizes = array();
+			$sizes[$this->id] = array($new_instance['thumb_w'], $new_instance['thumb_h']);
+			update_option('sk_cat_post_thumb_sizes', $sizes);
+		}
+        return $new_instance;
+	}
+}
+
+
+class SAOKIM_Walker_Category_Checklist extends Walker
+{
+	var $tree_type = 'category';
+	var $db_fields = array('parent'=>'parent', 'id'=>'term_id'); //TODO: decouple this
+	var $number;
+
+	function start_lvl (&$output, $depth, $args)
+	{
+		$indent = str_repeat("\t", $depth);
+		$output .= "$indent<ul class='children'>\n";
+	}
+
+	function end_lvl (&$output, $depth, $args)
+	{
+		$indent = str_repeat("\t", $depth);
+		$output .= "$indent</ul>\n";
+	}
+
+	function start_el (&$output, $category, $depth, $args)
+	{
+		extract($args);
+		
+		$class = in_array($category->term_id, $popular_cats) ? ' class="popular-category"' : '';
+		$output .= "\n<li id='category-$category->term_id-$this->number'$class>" . '<label class="selectit"><input value="' . $category->term_id . '" type="checkbox" name="post_category[]" id="post_category_' . $category->term_id . '"' . (in_array($category->term_id, $selected_cats) ? ' checked="checked"' : "") . '/> ' . wp_specialchars(apply_filters('the_category', $category->name)) . '</label>';
+	}
+
+	function end_el (&$output, $category, $depth, $args)
+	{
+		$output .= "</li>\n";
+	}
+}
+
+/**
+ * Creates the categories checklist
+ *
+ * @param int $post_id
+ * @param int $descendants_and_self
+ * @param array $selected_cats
+ * @param array $popular_cats
+ * @param int $number
+ */
+function saokim_wp_category_checklist ($post_id = 0, $descendants_and_self = 0, $selected_cats = false, $popular_cats = false)
+{
+	$walker = new SAOKIM_Walker_Category_Checklist();
+	
+	$descendants_and_self = (int) $descendants_and_self;
+	
+	$args = array();
+	if (is_array($selected_cats))
+		$args['selected_cats'] = $selected_cats;
+	elseif ($post_id)
+		$args['selected_cats'] = wp_get_post_categories($post_id);
+	else
+		$args['selected_cats'] = array();
+	
+	if (is_array($popular_cats))
+		$args['popular_cats'] = $popular_cats;
+	else
+		$args['popular_cats'] = get_terms('category', array('fields'=>'ids', 'orderby'=>'count', 'order'=>'DESC', 'number'=>10, 'hierarchical'=>false));
+	
+	if ($descendants_and_self) {
+		$categories = get_categories("child_of=$descendants_and_self&hierarchical=0&hide_empty=0");
+		$self = get_category($descendants_and_self);
+		array_unshift($categories, $self);
+	} else {
+		$categories = get_categories('get=all');
+	}
+	
+	// Post process $categories rather than adding an exclude to the get_terms() query to keep the query the same across all posts (for any query cache)
+	$checked_categories = array();
+	for ($i = 0; isset($categories[$i]); $i ++) {
+		if (in_array($categories[$i]->term_id, $args['selected_cats'])) {
+			$checked_categories[] = $categories[$i];
+			unset($categories[$i]);
+		}
+	}
+	
+	// Put checked cats on top
+	echo call_user_func_array(array(&$walker, 'walk'), array($checked_categories, 0, $args));
+	// Then the rest of them
+	echo call_user_func_array(array(&$walker, 'walk'), array($categories, 0, $args));
+}
+
+add_action( 'widgets_init', create_function('', 'return register_widget("Category_Recent_posts");') );
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Plugin Name ===
+Contributors: Nguyen Duc Manh
+Donate link: http://www.somihanquoc.vn/
+Tags: categories, posts, widget, checked categories post
+Requires at least: 2.8
+Tested up to: 3.0
+Stable tag: 3.2
+
+Add a widget that shows the most recent posts in checked categories
+
+== Description ==
+
+Categories Recent Posts Widget is a light widget designed to do one thing and do it well: display the most recent posts from checked  categories. See more about author of this plugin: http://casanova.vn
+
+Features:
+
+* [NEW] Option to change ordering of posts.
+* Support for displaying thumbnail images via WP 2.9's new post thumbnail feature.
+* Set how many posts to show.
+* Can checked categories that posts come from
+* Multiple widgets.
+
+== Installation ==
+
+1. Download the plugin.
+2. Upload it to the plugins folder of your blog.
+3. Goto the Plugins section of the WordPress admin and activate the plugin.
+4. Goto the Widget tab of the Presentation section and configure the widget.
+
+== Upgrade Notice ==
+Note that version 3.0 drops support for [Simple Post Thumbnails plugin](http://wordpress.org/extend/plugins/simple-post-thumbnails/) in favor of WP 2.9's built in post thumbnail functionality.
+
+== Screenshots ==
+
+1. The widget configuration dialog.
+
+== Changelog ==
+
+2.0
+* Updated to use the WP 2.8 widget API.
+* Added support for [Simple Post Thumbnails plugin](http://wordpress.org/extend/plugins/simple-post-thumbnails/).
+
+== Frequently Asked Questions ==
+Ask me by email
